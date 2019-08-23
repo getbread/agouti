27caf1a7c2a7057858004946aaaf14f8ff4d6604
Caleb Doxsey <caleb@doxsey.net>
2019-08-23 10:31:52 -0600

    try again

--- api/session.go
+++ api/session.go
@@ -229,7 +229,7 @@ func (s *Session) Frame(frame *Element) error {
 		elementID = struct {
 			Element    string `json:"ELEMENT"`
 			W3CElement string `json:"element-6066-11e4-a52e-4f735466cecf"`
-		}{frame.ID}
+		}{frame.ID, frame.ID}
 	}
 
 	request := struct {
